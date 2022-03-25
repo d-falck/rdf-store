cc9e577f62f96aec89bce428c69ad0373193ce8c
Damon Falck <damon.falck@worc.ox.ac.uk>
2022-03-25 18:42:13 +0000

    Remove old hash function code

--- include/hash.h
+++ include/hash.h
@@ -17,24 +17,3 @@ struct std::hash<std::tuple<TT...>> {
         return seed;
     }
 };
-
-// // Recursive helper
-// template <class Tuple, size_t index=std::tuple_size<Tuple>::value-1>
-// void recursive_combine(std::size_t& seed, const Tuple& tuple) {
-//     hash_combine(seed, std::get<index>(tuple));
-//     recursive_combine<Tuple, index-1>(seed, tuple);
-// }
-// template <class Tuple>
-// void recursive_combine<Tuple,0>(std::size_t& seed, const Tuple& tuple) {
-//     hash_combine(seed, std::get<0>(tuple));
-// }
-
-// // Inject custom hash function for tuples into std
-// template<typename ...TT>
-// struct std::hash<std::tuple<TT...>> {
-//     std::size_t operator()(const std::tuple<TT...>& tuple) const noexcept {
-//         size_t seed = 0;
-//         recursive_combine<std::tuple<TT...>>(seed, tuple);
-//         return seed;
-//     }
-// };
